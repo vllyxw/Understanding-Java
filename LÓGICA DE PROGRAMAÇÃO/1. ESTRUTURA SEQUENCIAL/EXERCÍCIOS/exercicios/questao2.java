7109dc6c25ca2a678c891fbc9109cadeab0ac417
vllyxw <victoriapansini661@gmail.com>
2024-10-10 20:25:19 -0300

    main

---
+++ LÓGICA DE PROGRAMAÇÃO/1. ESTRUTURA SEQUENCIAL/EXERCÍCIOS/exercicios/questao2.java
@@ -0,0 +1,22 @@
+package exercicios;
+
+import java.util.Scanner;
+
+public class questao2 {
+  public static void main(String[] args) {
+    Scanner sc = new Scanner(System.in);
+
+    double pi = 3.14159;
+    double raio;
+    double area;
+
+    System.out.println("Digite o tamanho do raio: ");
+    raio = sc.nextDouble();
+
+    area = pi * Math.sqrt(raio);
+
+    System.out.println("O valor da área é " + area);
+    
+    sc.close();
+}
+}
