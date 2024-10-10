c7ec2011c8c3e312bbe1478e3bfd2f76027fc601
vllyxw <victoriapansini661@gmail.com>
2024-10-10 20:59:17 -0300

    main

---
+++ LÓGICA DE PROGRAMAÇÃO/1. ESTRUTURA SEQUENCIAL/EXERCÍCIOS/exercicios/questao4.java
@@ -0,0 +1,28 @@
+package exercicios;
+
+import java.util.Scanner;
+
+public class questao4 {
+ public static void main(String[] args) {
+     Scanner sc = new Scanner(System.in);
+
+     int numero, horas;
+     float vlr, salario;
+
+     System.out.println("Digite seu número: ");
+     numero = sc.nextInt();
+
+     System.out.println("Digite suas horas trabalhadas: ");
+     horas = sc.nextInt();
+
+     System.out.println("Digite o valor da hora: ");
+     vlr = sc.nextFloat();
+
+     salario = horas * vlr;
+
+     System.out.println("Número: " + numero);
+     System.out.println("Salário: " + salario);
+
+     sc.close();
+ }
+}
