bf114589b5ab047120936ce875cd1bfa49c9725a
vllyxw <victoriapansini661@gmail.com>
2024-10-18 15:38:26 -0300

    main

--- 1. LÓGICA DE PROGRAMAÇÃO/2. ESTRUTURA CONDICIONAL/EXERCÍCIOS/exercicio_2/Main.java
+++ 1. LÓGICA DE PROGRAMAÇÃO/2. ESTRUTURA CONDICIONAL/EXERCÍCIOS/exercicio_2/Main.java
@@ -1,3 +1,18 @@
-public class Main {
+import java.util.Scanner;
 
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Digite um número:");
+        int num = scanner.nextInt();
+
+        if (num % 2 == 0) {
+            System.out.println("Este número é par");
+        } else{
+            System.out.println("Este número é ímpar");
+        }
+
+        scanner.close();
+    }
 }
