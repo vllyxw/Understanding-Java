1f3b6a75a9ecdf5914f7cf19fa2b34ee13daa02b
vllyxw <victoriapansini661@gmail.com>
2024-10-18 20:06:30 -0300

    main

--- 1. LÓGICA DE PROGRAMAÇÃO/4. EXERCÍCIOS/02. LISTA 02/Q2/Main.java
+++ 1. LÓGICA DE PROGRAMAÇÃO/4. EXERCÍCIOS/02. LISTA 02/Q2/Main.java
@@ -1,3 +1,20 @@
+import java.util.Scanner;
+
 public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.printf("Digite um número: ");
+        int num = scanner.nextInt();
+
+        if (num < 0) {
+            System.out.printf("Este número é negativo");
+        } else {
+            System.out.printf("Este número é positivo");
+        }
+
+        scanner.close();
+
+    }
 
 }
