91577b39ce2a5b0ff8a14358eda2c5b78c68f1d1
vllyxw <victoriapansini661@gmail.com>
2024-10-18 14:35:54 -0300

    main

---
+++ 1. LÓGICA DE PROGRAMAÇÃO/2. ESTRUTURA CONDICIONAL/EXERCÍCIOS/exercicio_1/Main.java
@@ -0,0 +1,20 @@
+
+import java.util.Scanner;
+
+public class Main {
+public static void main(String[] args) {
+    Scanner scanner = new Scanner(System.in);
+
+    System.out.println("Digite um numero: ");
+    int num = scanner.nextInt();
+
+    if(num < 0) {
+        System.out.println("Este numero é negativo");
+    }
+    else{
+        System.out.println("Este numero é positivo");
+    }
+
+    scanner.close();
+}
+}
