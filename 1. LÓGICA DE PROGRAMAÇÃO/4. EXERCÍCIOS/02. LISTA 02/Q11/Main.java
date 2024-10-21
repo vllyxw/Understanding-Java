8791be00b85fecab476c5ae9a3b24bebddb0a956
vllyxw <victoriapansini661@gmail.com>
2024-10-21 17:15:23 -0300

    main

--- 1. LÓGICA DE PROGRAMAÇÃO/4. EXERCÍCIOS/02. LISTA 02/Q11/Main.java
+++ 1. LÓGICA DE PROGRAMAÇÃO/4. EXERCÍCIOS/02. LISTA 02/Q11/Main.java
@@ -4,6 +4,50 @@ public class Main {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
+        System.out.printf("Calculadora");
+        System.out.println("Escolha uma opção:");
+        System.out.println("[1] - Adição");
+        System.out.println("[2] - Subtração");
+        System.out.println("[3] - Multiplicação");
+        System.out.println("[4] - Divisão");
+        System.out.println("[5] - Fim");
+        System.out.printf("Escolha uma opção: ");
+        int opcao = scanner.nextInt();
+
+        System.out.printf("Digite o primeiro número: ");
+        int num1 = scanner.nextInt();
+
+        System.out.printf("Digite o segundo número: ");
+        int num2 = scanner.nextInt();
+
+        int total;
+
+        switch (opcao) {
+            case 1: 
+                total = num1 + num2;
+                System.out.printf("A adição ficou: %d", total);
+                break;
+            case 2:
+                total = num1 - num2;
+                System.out.printf("A subtração ficou: %d", total);
+                break;
+            case 3:
+                total = num1 * num2;
+                System.out.printf("A multiplição ficou: %d", total);
+                break;
+            case 4:
+                total = num1 / num2;
+                System.out.printf("A divisão ficou: %d", total);
+                break;
+            case 5: 
+                System.out.println("Adeus...");
+                break;
+            default:
+                System.out.println("Opção inválida");
+                break;
+        }
+
+        scanner.close();
         
     }
 
