03aa81eb045e6c682ba10cfe85c44bf43c99a468
vllyxw <victoriapansini661@gmail.com>
2024-10-24 18:16:26 -0300

    main

--- 1. LÓGICA DE PROGRAMAÇÃO/4. EXERCÍCIOS/02. LISTA 02/Q22/Main.java
+++ 1. LÓGICA DE PROGRAMAÇÃO/4. EXERCÍCIOS/02. LISTA 02/Q22/Main.java
@@ -24,7 +24,85 @@ public class Main {
 
                 switch (tipo) {
                     case 1:
+                        System.out.printf("1 - Carnívoro \n");
+                        System.out.printf("2 - Herbívoro \n");
+                        int escolha1 = scanner.nextInt();
+
+                        if (escolha1 == 1) {
+                            System.out.printf("Leão");
+                        } else {
+                            System.out.printf("Cavalo");
+                        }
+                        break;
+                    case 2: 
+                        System.out.printf("1 - Onívoro \n");
+                        System.out.printf("2 - Frutífero \n");
+                        int escolha2 = scanner.nextInt();
+
+                        if (escolha2 == 1) {
+                            System.out.printf("Homem");
+                        } else if (escolha2 == 2) {
+                            System.out.printf("Macaco");
+                        } else {
+                            System.out.printf("Valor Inválido");
+                        }
+                        break;
+                    case 3:
+                        System.out.printf("Morcego");
+                        break;
+                    case 4:
+                        System.out.printf("Baleia");
+                        break;
+                    default:
+                        System.out.printf("Valor inválido");
+                        break;
                 }
+            case 2: 
+                System.out.printf("1 - Não voadoras \n");
+                System.out.printf("2 - Nadadoras \n");
+                System.out.printf("3 - de Rapina \n");
+                int aves = scanner.nextInt();
+
+                if (aves == 1) {
+                    System.out.printf("1 - Tropical \n");
+                    System.out.printf("2 - Polar \n");
+                    int polaridade = scanner.nextInt();
+
+                    if(polaridade == 1) {
+                        System.out.printf("Avestruz");
+                    } else if (polaridade == 2) {
+                        System.out.printf("Pinguim");
+                    } else {
+                        System.out.printf("Valor inválido");
+                    }
+                }else if (aves == 2) {
+                    System.out.printf("Pato");
+                } else if (aves == 3) {
+                    System.out.printf("Águia");
+                } else {
+                    System.out.printf("Valor inválido");
+                }
+                break;
+            case 3:
+                System.out.printf("1 - Com casco \n");
+                System.out.printf("2 - Carnívoros \n");
+                System.out.printf("3 - Sem patas \n");
+                System.out.printf("Escolha a opção: ");
+                int repteis = scanner.nextInt();
+
+                if (repteis == 1) {
+                    System.out.printf("Tartaruga");
+                } else if (repteis == 2) {
+                    System.out.printf("Crocodilo");
+                } else if (repteis == 3) {
+                    System.out.printf("Cobra");
+                } else {
+                    System.out.printf("Valor inválido");
+                }
+                break;
+            default:
+                System.out.printf("Valor Inválido");
+                break;
         }
     }
 
